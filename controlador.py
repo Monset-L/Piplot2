7f58f67d32444f93107f1946905090e423a94635
DaniDoom35 <chargerpod@gmail.com>
2023-03-13 03:59:19 -0600

    Ahora si sirve
    Se trato de hacer algo despues de que se me borrara D:

--- controlador.py
+++ controlador.py
@@ -1,3 +1,25 @@
-from taskList import *
-piplotObj = taskList("piplot")
-piplotObj()
+
+from tkinter import *
+import tkinter as tk
+
+
+class tareas ():
+    
+    def __init__(self):
+        self.tarea = tarea
+
+    def agregar_tarea(entrada):
+    
+        print(entrada)
+        
+    def ver_tarea():
+        
+        ventana = tk.Tk()
+
+
+
+
+        #listbox = tk.Listbox(ventana, width=30)
+        #listbox.pack()
+
+        ventana.mainloop()
--- taskList.py
+++ taskList.py
@@ -1,19 +1,43 @@
-from tkinter import *
+import tkinter as tk
+from controlador import *
 
-class taskList:
-    
-    def __init__(self, usuario):
-        self.usuario = "piplot"
 
-    ventana = Tk()
-    ventana.title = "Piplot"
-    ventana.geometry="600x400"
-    ventana.configure(background="#B8FFAB")
+
+ventana = tk.Tk() 
     
-    seccion1 = Frame(ventana, bg="#B8FFAB")
-    seccion1.configure(height=10)
+def agregar ():
+    asd.agregar_tarea(entrada.get())
+    
+def ver():
+    
+    asd.ver_tarea()
+    
+asd = tareas
     
     
-    seccion2 = Frame(ventana, bg="#B8FFCC")
+
+ventana.title = "Piplot"
+ventana.geometry="600x400"
+ventana.configure(background="#B8FFAB")
     
-    ventana.mainloop()
+seccion1 = Frame(ventana, bg="#B8FFAB")
+seccion1.configure(height=10)
+    
+entrada = tk.Entry(ventana)
+entrada.pack()
+    
+boton_agregar = tk.Button(ventana, text="Agregar",command=agregar)
+boton_agregar.pack()
+
+boton_ver = tk.Button(ventana, text="Ver tarea",command=ver)
+boton_ver.pack()
+
+
+
+
+
+seccion2 = Frame(ventana, bg="#B8FFCC")
+
+ 
+    
+ventana.mainloop()
