daa1733fd05fc6c95a4a2ee11bb6fa1ca90d2256
Fyrewalkwithme <loeza.garcia.luisarturo@gmail.com>
2023-03-13 11:30:15 -0600

    Añadí tabla y formato de ingreso de tarea

--- taskList.py
+++ taskList.py
@@ -1,30 +1,61 @@
 from tkinter import *
+from tkinter import ttk
 from controlador import *
+from Ingreso import *
 
-ventana = Tk() 
-ventana.title =("Piplot")
-ventana.geometry=("600x400")
-ventana.configure(background="#B8FFAB")
-    
-seccion1 = Frame(ventana, bg="#B8FFAB")
-seccion1.pack(expand=True, fill='both')
-      
+ventanam = Tk() 
+ventanam.title("Piplot")
+ventanam.geometry("700x400")
+ 
+controltareas = tareas()
+intIngreso = ingreso()
+   
+s1 = Frame(ventanam, bg="#B8FFAB")
+s1.pack(fill='both', expand=True)
+
+welcome = Label(s1, text="Bienvenido a Piplot", bg="#B8FFAB", font="Lucida 18 bold")
+welcome.pack(pady=10)
+
+tabla = ttk.Treeview(s1)
+tabla['columns'] = ('Nombre', 'Num', 'Descrip', 'Inicio', 'Fin')
+
+tabla.column("#0", width=0, stretch=NO)
+tabla.column("Nombre", anchor=CENTER, width=80)
+tabla.column("Num", anchor=CENTER, width=80)
+tabla.column("Descrip", anchor=CENTER, width=80)
+tabla.column("Inicio", anchor=CENTER, width=80)
+tabla.column("Fin", anchor=CENTER, width=80)
+
+tabla.heading("#0", text="", anchor=CENTER)
+tabla.heading("Nombre", text="Nombre", anchor=CENTER)
+tabla.heading("Num", text="Número", anchor=CENTER)
+tabla.heading("Descrip", text="Descripción", anchor=CENTER)
+tabla.heading("Inicio", text="Inicio", anchor=CENTER)
+tabla.heading("Fin", text="Fin", anchor=CENTER)
+
+tabla.insert(parent='',index='end',iid=0,text='',
+values=('1','Física Ej.','Espejos Esféricos','3/10/23', '3/13/23'))
+tabla.insert(parent='',index='end',iid=1,text='',
+values=('2','Mate Ejercicios','Derivs. Parciales','3/9/23', '3/14/23'))
+tabla.insert(parent='',index='end',iid=2,text='',
+values=('3','Expo Ética','Valores','3/10', '3/14/23'))
+
+tabla.pack(pady=5)
+
 def agregar ():
-    asd.agregar_tarea(entrada.get())
+    controltareas.agregar_tarea(entrada.get())
     
 def ver():
     
-    asd.ver_tarea()
+    controltareas.ver_tarea()
     
-asd = tareas()
+entrada = Entry(s1)
+entrada.place(x=150, y=300)
     
-entrada = Entry(seccion1)
-entrada.pack()
-    
-boton_agregar = Button(seccion1, text="Agregar",command=agregar)
-boton_agregar.pack()
+boton_agregar = Button(s1, text="Agregar",command=agregar)
+boton_agregar.place(x=410, y=295)
 
-boton_ver = Button(seccion1, text="Ver tarea",command=ver)
-boton_ver.pack()
+boton_ver = Button(s1, text="Ver tarea",command=ver)
+boton_ver.place(x=480,y=295)
 
-ventana.mainloop()
+ventanam.mainloop()
